08b23061786710450ced37565482d7e4e6d23c3b
Dave Kindel <dkindel@vt.edu>
2014-10-15 22:56:20 -0400

    commit with broken code

---
+++ gui/mygraphicsview.h
@@ -0,0 +1,18 @@
+#ifndef MYGRAPHICSVIEW_H
+#define MYGRAPHICSVIEW_H
+
+#include <QGraphicsView>
+
+class MyGraphicsView : public QGraphicsView
+{
+    Q_OBJECT
+public:
+    explicit MyGraphicsView(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // MYGRAPHICSVIEW_H
